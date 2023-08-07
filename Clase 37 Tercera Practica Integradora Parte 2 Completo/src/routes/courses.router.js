d08f35e285cd779a9e20b60321b2041226923777
Andrea1904 <alopezm@bancodeoccidente.com.co>
2023-08-07 17:00:30 -0500

    Add files via upload

---
+++ Clase 37 Tercera Practica Integradora Parte 2 Completo/src/routes/courses.router.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import passport from 'passport';
+import coursesController from '../controllers/courses.controller.js';
+import applyPolicy from '../middleware/auth.middleware.js';
+const router = Router();
+router.use(passport.authenticate('current',{session:false}))
+
+//router.get ('/',courseController.getCourses)
+router.get('/',applyPolicy(['STUDENT']),coursesController.getCourses)
+
+//router.post('/',courseController.createCourse)
+router.post('/',applyPolicy(['TEACHER']),coursesController.createCourse);
+
+export default router;
